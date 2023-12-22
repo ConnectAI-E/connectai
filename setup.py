89b403b44ba698e24f32357cab22022d861bc48d
lloydzhou <lloydzhou@qq.com>
2023-12-22 14:50:30 +0800

    feat: get version in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
+import codecs
 import copy
 import glob
 import os.path
 import runpy
 import sys
+
+
+def read(rel_path):
+    with codecs.open(rel_path, "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 root_folder = os.path.abspath(os.path.dirname(__file__))
 packages = [f.split(os.sep)[1] for f in glob.glob("src/*/setup.py")]
@@ -18,7 +34,7 @@ for package in packages:
         sys.path = [pkg_setup_folder] + copy.copy(saved_syspath)
 
         print("Start ", pkg_setup_path)
-        result = runpy.run_path(pkg_setup_path)
+        result = runpy.run_path(pkg_setup_path, init_globals=globals())
     except Exception as e:
         print(e, file=sys.stderr)
     finally:
